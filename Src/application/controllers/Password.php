44f9f354a481cb6206270e8de9cf49994909edb0
Prasetyo S <prasetyosudarji@gmail.com>
2018-03-10 23:21:51 +0700

    - Fixed forgot password via smtp hostinger

--- Src/application/controllers/Password.php
+++ Src/application/controllers/Password.php
@@ -21,11 +21,12 @@ class Password extends CI_Controller {
 				$this->load->view('template/wrapper', $data);
     		}else{
     			$email = null;
+    			$username = $inputUsername;
     			foreach ($queryUser as $key => $value) {
     				# code...
     				$email = $value['email'];
     			}
-    			$this->sendRecoveryEmail($email);
+    			$this->sendRecoveryEmail($email,$username,$this->randomPassword());
     		}
     	}else if($inputEmail!=null){
     		$queryUserEmail = $this->Model->ambil("email",$inputEmail,"tbl_user")->result_array();
@@ -42,43 +43,66 @@ class Password extends CI_Controller {
 				$this->load->view('template/wrapper', $data);
 			}else{
 				$email = null;
+				$username = null;
     			foreach ($queryUserEmail as $key => $value) {
     				# code...
     				$email = $value['email'];
+    				$username = $value['email'];
     			}
-    			$this->sendRecoveryEmail($email);
+    			$this->sendRecoveryEmail($email,$username,$this->randomPassword());
 			}
+    	}else{
+    		$alert = "<script>
+						alert('Username/Email tidak valid!!');
+						window.location.href='".base_url()."';
+						</script>";
+				$data = array(
+					'alert' => $alert,
+					'page' => 'notification',
+					'link' => 'home'
+				);
+				$this->load->view('template/wrapper', $data);
     	}
     }
 
-    public function sendRecoveryEmail($toEmail){
-    	/* config sendMail
-    	$config['protocol'] = 'sendmail';
-		$config['mailpath'] = '/usr/sbin/sendmail';
-		$config['charset'] = 'iso-8859-1';
-		$config['wordwrap'] = TRUE;
-		*/
+    public function sendRecoveryEmail($toEmail,$username,$newPass){
+    	$config = array(
+		    'protocol' => 'smtp',
+		    'smtp_host' => 'mx1.hostinger.co.id',
+		    'smtp_port' => 587,
+		    'smtp_user' => 'support@bkcbdg.com',
+		    'smtp_pass' => 'p45sw0rd',
+		    'mailtype'  => 'html', 
+		    'charset'   => 'iso-8859-1'
+		);
+		
+		$this->load->library('email', $config);
+		$this->email->set_newline('\r\n');
 
-		/*$config['protocol'] = 'smtp';
-		$config['smtp_host']='ssl://smtp.mailgun.org';
-		$config['smtp_user']='postmaster@cpatt.mailgun.org';
-		$config['smtp_pass']='youpassword';
-		$config['smtp_port']='465';
+    	$this->email->from('support@bkcbdg.com', 'SUPPORT SAMSAK');
+		$listEmail = array($toEmail);
+        $this->email->to($listEmail);
+        $this->email->subject('[SAMSAK] Password Reset');
+		$this->email->message('Please do not reply to this email. This email is used only to help you remember your account. This is the detail of your account:
 
-		$this->email->initialize($config);
+Username :'.$username.'
+Password :'.$newPass);
+        $alert = "";
+        if ($this->email->send()) {
+        	$dataUpdate = array(
+        		'username' => md5($username),
+				'password' => md5($newPass)
+			);
 
-    	
-    	$this->email->from('your@example.com', 'Admin');
-		$this->email->to($toEmail);
-		$this->email->subject('[Samsak.id] Password Reset');
-		$this->email->message('Testing the email class.');
-
-		$this->email->send();
-		*/
-		$alert = "<script>
-		alert('Please check your e-mail to get your new password !!');
-		window.location.href='".base_url()."';
-		</script>";
+			$queryUpdate = $this->Model->update("email",$toEmail,"tbl_user",$dataUpdate);
+            $alert = "<script>
+			alert('Please check your e-mail to get your new password !!');
+			window.location.href='".base_url()."';
+			</script>";
+        } else {
+            show_error($this->email->print_debugger());
+        }
+		
 		$data = array(
 			'alert' => $alert,
 			'page' => 'notification',
@@ -180,5 +204,16 @@ class Password extends CI_Controller {
 			}
 		}
     }
+
+    public function randomPassword() {
+	    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+	    $pass = array(); //remember to declare $pass as an array
+	    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+	    for ($i = 0; $i < 8; $i++) {
+	        $n = rand(0, $alphaLength);
+	        $pass[] = $alphabet[$n];
+	    }
+	    return implode($pass); //turn the array into a string
+	}
 	
 }
